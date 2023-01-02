f3c0b931f9ec53ae7f4182cdd1c4ef0651dcfa90
Daniel Powley <danpowley@gmail.com>
2023-01-02 21:53:25 +0000

    Structure players list based on player number

--- src/fumbbl/pages/teammanagement/components/Team.vue
+++ src/fumbbl/pages/teammanagement/components/Team.vue
@@ -24,67 +24,72 @@
             </tr>
             </thead>
             <tbody>
-            <tr v-for="(player, index) in team.players" :key="index">
+            <tr v-for="(player, playerNumber) in playersInPositions" :key="playerNumber">
                 <td>
-                    {{ player.number }}
+                    {{ playerNumber }}
                 </td>
-                <td>
-                    <img src="x.jpg">
-                </td>
-                <td>
-                    <div>{{ player.name }} ({{ player.gender }})</div>
-                    <div>{{ player.position }}</div>
-                </td>
-                <td>
-                    {{ team.positionsLookup[player.positionId].stats.MA }}
-                </td>
-                <td>
-                    {{ team.positionsLookup[player.positionId].stats.ST }}
-                </td>
-                <td>
-                    {{ team.positionsLookup[player.positionId].stats.AG }}+
-                </td>
-                <td>
-                    {{ team.positionsLookup[player.positionId].stats.PA }}+
-                </td>
-                <td>
-                    {{ team.positionsLookup[player.positionId].stats.AV }}+
-                </td>
-                <td>
-                    <div>
-                        {{ team.positionsLookup[player.positionId].skills.join(', ') }}
-                    </div>
-                    <div>
-                        {{ player.skills.join(', ') }}
-                    </div>
-                </td>
-                <td>
-                    {{ player.injuries }}
-                </td>
-                <td>
-                    ?
-                </td>
-                <td>
-                    {{ player.record.completions }}
-                </td>
-                <td>
-                    {{ player.record.touchdowns }}
-                </td>
-                <td>
-                    {{ player.record.interceptions }}
-                </td>
-                <td>
-                    {{ player.record.casualties }}
-                </td>
-                <td>
-                    {{ player.record.mvps }}
-                </td>
-                <td>
-                    {{ player.record.spp }}
-                </td>
-                <td>
-                    {{ team.positionsLookup[player.positionId].cost }}
-                </td>
+                <template v-if="player !== null">
+                    <td>
+                        <img src="x.jpg">
+                    </td>
+                    <td>
+                        <div>{{ player.name }} ({{ player.gender }})</div>
+                        <div>{{ player.position }}</div>
+                    </td>
+                    <td>
+                        {{ team.positionsLookup[player.positionId].stats.MA }}
+                    </td>
+                    <td>
+                        {{ team.positionsLookup[player.positionId].stats.ST }}
+                    </td>
+                    <td>
+                        {{ team.positionsLookup[player.positionId].stats.AG }}+
+                    </td>
+                    <td>
+                        {{ team.positionsLookup[player.positionId].stats.PA }}+
+                    </td>
+                    <td>
+                        {{ team.positionsLookup[player.positionId].stats.AV }}+
+                    </td>
+                    <td>
+                        <div>
+                            {{ team.positionsLookup[player.positionId].skills.join(', ') }}
+                        </div>
+                        <div>
+                            {{ player.skills.join(', ') }}
+                        </div>
+                    </td>
+                    <td>
+                        {{ player.injuries }}
+                    </td>
+                    <td>
+                        ?
+                    </td>
+                    <td>
+                        {{ player.record.completions }}
+                    </td>
+                    <td>
+                        {{ player.record.touchdowns }}
+                    </td>
+                    <td>
+                        {{ player.record.interceptions }}
+                    </td>
+                    <td>
+                        {{ player.record.casualties }}
+                    </td>
+                    <td>
+                        {{ player.record.mvps }}
+                    </td>
+                    <td>
+                        {{ player.record.spp }}
+                    </td>
+                    <td>
+                        {{ team.positionsLookup[player.positionId].cost }}
+                    </td>
+                </template>
+                <template v-else>
+                    <td colspan="17">No player here</td>
+                </template>
             </tr>
             </tbody>
         </table>
@@ -108,7 +113,24 @@ import Component from 'vue-class-component';
 })
 export default class TeamComponent extends Vue {
     public positionsLookup: any = null;
+    public playersInPositions: any = null;
+
     async mounted() {
+        this.refreshPlayersInPositions();
+    }
+
+    public refreshPlayersInPositions() {
+        const playersInPositions = {};
+
+        for (let step = 1; step <= this.$props.team.ruleset.maxPlayers; step++) {
+            playersInPositions[step] = null;
+        }
+
+        for (const player of this.$props.team.players) {
+            playersInPositions[player.number] = player;
+        }
+
+        this.playersInPositions = playersInPositions;
     }
 }
 </script>
